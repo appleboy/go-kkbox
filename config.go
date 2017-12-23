1291988acadf091d51bc62d7b8a0eea335a9155b
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-12-23 10:50:51 +0800

    fix: comment error.

--- config.go
+++ config.go
@@ -6,9 +6,9 @@ const (
 
 	// ChartURL List of song rankings.
 	ChartURL = "https://api.kkbox.com/v1.1/charts"
-	// ChartPlayList retrieve information of the song ranking
+	// ChartPlayListURL retrieve information of the song ranking
 	// with {playlist_id}.
 	ChartPlayListURL = ChartURL + "/%s"
-	// ChartPlayListTrack list tracks of a chart playlist.
+	// ChartPlayListTrackURL list tracks of a chart playlist.
 	ChartPlayListTrackURL = ChartURL + "/%s/track"
 )
